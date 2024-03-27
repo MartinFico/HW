3a405b92089a57452b4657cb1805b651f3444118
Martin Fico <martin.fico@seznam.cz>
2024-03-27 09:39:16 +0100

    Vytištění a přečtení

--- test.py
+++ test.py
@@ -2,3 +2,6 @@ def main():
     file_name = "text.txt"  # Název vašeho souboru
     try:
         with open(file_name, "r", encoding="utf-8") as file:
+        original_text = file.read()
+        print("Původní text:")
+        print(original_text)
